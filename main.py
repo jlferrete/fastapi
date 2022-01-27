5775c703a14f1af3697e3efc08f932e0e277b923
Jlferrete <joseluisferrete@gmail.com>
2022-01-27 20:31:53 +0100

    Added Create Post and Get a Post

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,15 +12,33 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": 
+"favourite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_post():
-    return { "data": "This is your posts"}
+    return { "data": my_posts}
 
-@app.post("/createposts")
-def create_posts(new_post: Post): 
-    print(new_post.rating)
-    return{"data": "new_post"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000000) 
+    my_posts.append(post_dict)
+    return{"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id : int):
+    
+    post = find_post(int(id))
+    print(post)
+    return {"post detail": post}
