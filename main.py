9d5e944747c5d656ccedf340d2a424427ebacd33
Jlferrete <joseluisferrete@gmail.com>
2022-02-06 12:24:34 +0100

    Added delete post

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def find_post(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
@@ -50,4 +56,16 @@ def get_post(id : int):
         #return {'message' : f"post with id: {id} was not found"}
     return {"post detail": post}
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #deleting post
+    #find the index in the array that has required ID
+    #my_post.pop(index)
+    index = find_index_post(id)
 
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
